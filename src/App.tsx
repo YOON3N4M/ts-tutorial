8ee8d91e68e84e08535ee4d55fb16f8505605386
yoon3nam <lgmysn2@gmail.com>
2023-03-16 20:50:46 +0900

    파일 정리 및 styled-components 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import Counter from "./Counter";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body{
+  padding:0;
+  margin:0;
+}
+`;
 
 const App: React.FC = () => {
-  return <Counter />;
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Button danger={false} />
+        <Button danger />
+      </Container>
+    </>
+  );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`;
+
+const Button = styled.button<{ danger: boolean }>`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props: any) => (props.danger ? "#e74c3c" : "2ecc71")};
+`;
 
 export default App;
